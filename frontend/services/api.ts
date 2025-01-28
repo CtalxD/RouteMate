78418e2dee45f347d0e2300d7e27c953b6995daa
CtalxD <ctalaryal22@gmail.com>
2025-01-28 10:21:31 +0545

    created Axios instances

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -10,6 +10,14 @@ const api = axios.create({
   },
 });
 
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 // Add an interceptor for automatic token refresh
 api.interceptors.response.use(
   (response) => response,
