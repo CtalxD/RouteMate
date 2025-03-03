a3c8a066783c1026b7bd07f37ea02c3e2e96da47
CtalxD <ctalaryal22@gmail.com>
2025-03-03 20:46:14 +0545

    Comment out Map component import in lists.tsx

--- frontend/app/(tabs)/lists.tsx
+++ frontend/app/(tabs)/lists.tsx
@@ -1,10 +1,12 @@
 import ListHamburger from '@/components/list-hamburger';
+// import Map from "./map"
 import SafeAreaScrollableView from '@/components/safe-area-scrollable-view';
 
 const List = () => {
   return (
     <SafeAreaScrollableView>
       < ListHamburger/>
+      {/* <Map/> */}
     </SafeAreaScrollableView>
   );
 };
